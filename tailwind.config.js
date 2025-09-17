aec48bc20e521b077b57dd3548c7010582f95b84
zzhhaa <zzhhaa@protonmail.com>
2025-09-17 17:00:20 +0100

    feat: configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wasted_grey: "#rgb(119, 116, 116)",
+        wasted_green: "#rgb(71, 74, 30)",
+      },
+    },
+  },
+  plugins: [],
+}
