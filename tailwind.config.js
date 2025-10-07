79278585c645c82084f00148ed9f62cfbcf76912
zzhhaa <zzhhaa@protonmail.com>
2025-10-07 18:29:27 +0100

    feat(wip): split scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,48 @@ module.exports = {
     },
 
   },
-    plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          '&::-webkit-scrollbar': { display: 'none' },
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+        '.scrollbar-outline-camo': {
+          'scrollbar-width': 'thin',
+          'scrollbar-color': '#61623B00 #0000', // camo on transparent
+          '&::-webkit-scrollbar': {
+            width: '8px',
+            background: 'transparent',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'transparent',
+            border: '2px solid #61623B',
+            'border-radius': '8px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'transparent',
+          },
+        },
+        '.scrollbar-outline-grey': {
+          'scrollbar-width': 'thin',
+          'scrollbar-color': '#E8E6E700 #0000', // grey on transparent
+          '&::-webkit-scrollbar': {
+            width: '8px',
+            background: 'transparent',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'transparent',
+            border: '2px solid #E8E6E7',
+            'border-radius': '8px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'transparent',
+          },
+        },
+      });
+    }
+  ],
 }
